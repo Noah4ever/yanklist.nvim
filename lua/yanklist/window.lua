017158f89223b9e4a590bc31065be8c773a3025a
Noah4ever <noah.thiering@web.de>
2023-04-30 18:47:09 +0200

    feat: add getting yank history (not working correct)

--- lua/yanklist/window.lua
+++ lua/yanklist/window.lua
@@ -6,12 +6,28 @@ function M.open()
   local height = 10
 
   -- Create the scratch buffer displayed in the floating window
-  local bufnr = vim.api.nvim_create_buf(false, true)
+  local buf = vim.api.nvim_create_buf(false, true)
+
+  local yank_history = vim.fn.execute("reg")
+  -- Change yank_history to a table
+  yank_history = vim.split(yank_history, "\n")
+  -- Remove the first line
+  table.remove(yank_history, 1)
+  table.remove(yank_history, 2)
+  -- Get only the content of the registers
+  for i, v in ipairs(yank_history) do
+    print("asd", v)
+    yank_history[i] = vim.split(v, '"')[2]
+  end
+
+  print(vim.inspect(yank_history))
+
+  vim.api.nvim_buf_set_lines(buf, 0, -1, false, yank_history)
 
   -- Create the floating window
   local ui = vim.api.nvim_list_uis()[1]
 
-  local win = vim.api.nvim_open_win(bufnr, true, {
+  local win = vim.api.nvim_open_win(buf, true, {
     relative = "editor",
     width = width,
     height = height,
